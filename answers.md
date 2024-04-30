21f913280ba0b36f6400eb8e8010da72de7d140a
juliarenner <157319766+juliarenner@users.noreply.github.com>
2024-04-30 02:53:44 +0000

    Lab 08

--- answers.md
+++ answers.md
@@ -2,8 +2,7 @@
 
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:** Julia Renner
 
 
 Place all written answers from `recitation-08.md` here for easier grading.
@@ -11,8 +10,15 @@ Place all written answers from `recitation-08.md` here for easier grading.
 
 
 - **1b)**
-
-
-
+- 
+- The work of my algorithm is O(V+E * logV)
+- The span of my algorithm is O(V+E)
+- 
 - **2b)**
-
+- 
+- def get_path(parents, destination):
+  - path = []
+  - while destination != parents[destination]:
+    - destination = parents[destination]
+    - path.append(destination)
+  - return ''.join(reversed(path))
--- main.py
+++ main.py
@@ -1,48 +1,52 @@
 from collections import deque
-from heapq import heappush, heappop 
+from heapq import heappush, heappop
+
 
 def shortest_shortest_path(graph, source):
-    """
-    Params: 
-      graph.....a graph represented as a dict where each key is a vertex
-                and the value is a set of (vertex, weight) tuples (as in the test case)
-      source....the source node
-      
-    Returns:
-      a dict where each key is a vertex and the value is a tuple of
-      (shortest path weight, shortest path number of edges). See test case for example.
-    """
-    ### TODO
-    pass
-    
 
-    
-    
+  shortest_path = {}
+
+  for node in graph:
+    shortest_path[node] = (float('inf'), 0)
+
+  shortest_path[source] = (0, 0)
+  lst = deque([source])
+
+  while lst:
+    node = lst.popleft()
+    for neighbor, weight in graph[node]:
+      if shortest_path[neighbor][0] > shortest_path[node][0] + weight:
+        shortest_path[neighbor] = (shortest_path[node][0] + weight,
+                                   shortest_path[node][1] + 1)
+        lst.append(neighbor)
+  return shortest_path
+
+
 def bfs_path(graph, source):
-    """
-    Returns:
-      a dict where each key is a vertex and the value is the parent of 
-      that vertex in the shortest path tree.
-    """
-    ###TODO
-    pass
+
+  parents = {}
+
+  for node in graph:
+    parents[node] = None
+  parents[source] = source
+  lst = deque([source])
+  while lst:
+    node = lst.popleft()
+    for neighbor in graph[node]:
+      if parents[neighbor] is None:
+        parents[neighbor] = node
+        lst.append(neighbor)
+
+  return parents
+
 
 def get_sample_graph():
-     return {'s': {'a', 'b'},
-            'a': {'b'},
-            'b': {'c'},
-            'c': {'a', 'd'},
-            'd': {}
-            }
+  return {'s': {'a', 'b'}, 'a': {'b'}, 'b': {'c'}, 'c': {'a', 'd'}, 'd': {}}
 
 
-    
 def get_path(parents, destination):
-    """
-    Returns:
-      The shortest path from the source node to this destination node 
-      (excluding the destination node itself). See test_get_path for an example.
-    """
-    ###TODO
-    pass
-
+  path = []
+  while destination != parents[destination]:
+    destination = parents[destination]
+    path.append(destination)
+  return ''.join(reversed(path))
